2adbffd115aa7d0cb2f561786204d8d8a08e8748
Pierre <pierre@macbook-air-de-pierre.home>
2024-01-28 16:13:23 +0100

    fix: Modification des codes d'erreurs

--- src/models/vault-error.model.ts
+++ src/models/vault-error.model.ts
@@ -1,8 +1,8 @@
 export class VaultError{
 
     static OPEN_VAULT_READING_FAILED = new VaultError(500,"Erreur de lecture du fichier de stockage");
-    static OPEN_VAULT_NOT_FOUND = new VaultError(500,"Aucun coffre ne porte ce nom");
-    static OPEN_VAULT_BAD_PASSWORD= new VaultError(500,"Mot de passe incorrect");
+    static OPEN_VAULT_NOT_FOUND = new VaultError(404,"Aucun coffre ne porte ce nom");
+    static OPEN_VAULT_BAD_PASSWORD= new VaultError(401,"Mot de passe incorrect");
     static OPEN_VAULT_INTEGRITY_FAILED= new VaultError(500,"Erreur d'intégrité ! Votre coffre fort à été modifié");
 
 
